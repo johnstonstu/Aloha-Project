c7bd19d4a38328831422ebd88d16cd0257e78ab4
johnstonstu <stu.w.johnston@gmail.com>
2018-04-19 16:16:07 -0700

    Added js carousel and form function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
-console.log("this better work");
+$(document).ready(function() {
+  // $(function() {
+  //   // your jQuery code here...
+  // });
+
+  $(".main-carousel").flickity({
+    // options
+
+    wrapAround: true,
+    contain: true,
+    fullscreen: true,
+    lazyLoad: 1
+  });
+
+  $(".sub-button").on("click", function() {
+    // Code to run when button clicked...
+
+    var inputVal = $(".form-input").val();
+
+    if ($(".form-input").val() == "") {
+      alert("You missed the field.");
+    } else {
+      alert(inputVal);
+    }
+    console.log(inputVal);
+    // alert(inputVal);
+    console.log(event);
+    // console.log(input);
+  });
+
+  console.log("this better work");
+});
