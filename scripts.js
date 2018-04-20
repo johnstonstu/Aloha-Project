097b6821ccc368c78d26edaa2a9c98424baab7e5
johnstonstu <stu.w.johnston@gmail.com>
2018-04-19 23:42:49 -0700

    fixed a few text placement and began editing scaling issues

--- scripts.js
+++ scripts.js
@@ -1,32 +1,69 @@
 $(document).ready(function() {
-  // $(function() {
-  //   // your jQuery code here...
-  // });
-
+  // Carousel
   $(".main-carousel").flickity({
-    // options
-
     wrapAround: true,
     contain: true,
     fullscreen: true,
-    lazyLoad: 1
+    lazyLoad: 1,
+    prevNextButtons: false
   });
 
+  // Form submit
   $(".sub-button").on("click", function() {
     // Code to run when button clicked...
-
     var inputVal = $(".form-input").val();
 
     if ($(".form-input").val() == "") {
       alert("You missed the field.");
     } else {
-      alert(inputVal);
+      alert("Thanks for subscribing: " + inputVal);
     }
-    console.log(inputVal);
-    // alert(inputVal);
-    console.log(event);
-    // console.log(input);
   });
+
+  // Smooth scrolling as seen on https://css-tricks.com/snippets/jquery/smooth-scrolling/#article-header-id-1
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000,
+            function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
 
   console.log("this better work");
 });
