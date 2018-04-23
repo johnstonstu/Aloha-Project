0149c4e3a79934bf36bfed21c80b32430c532a27
johnstonstu <stu.w.johnston@gmail.com>
2018-04-23 00:23:27 -0700

    re-made email form button, still having scaling issues

--- scripts.js
+++ scripts.js
@@ -11,14 +11,21 @@ $(document).ready(function() {
   });
 
   // Form submit
-  $(".sub-button").on("click", function() {
+  $(".sub-button").on("click", function(event) {
     // Code to run when button clicked...
+    event.preventDefault();
     var inputVal = $(".form-input").val();
 
-    if ($(".form-input").val() == "") {
-      alert("You missed the field.");
-    } else {
+    // valadate email as seen on https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    function validateEmail(inputVal) {
+      var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+      return re.test(inputVal);
+    }
+
+    if (validateEmail(inputVal)) {
       alert("Thanks for subscribing: " + inputVal);
+    } else {
+      alert("Invalid Email. Please try again.");
     }
   });
 
